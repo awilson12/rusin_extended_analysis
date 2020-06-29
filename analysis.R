ffd63924f88a483f637534d4b9128ebb8d50bfa9
awilson12 <apfeifer@email.arizona.edu>
2020-06-29 15:34:55 -0700

    exploring relationship between inoculum and TE

--- analysis.R
+++ analysis.R
@@ -4,6 +4,7 @@ rusin<-read.csv('rusin_mouthte.csv')
 require(ggplot2)
 require(ggpubr)
 
+#overall distribution of TE
 ggplot(rusin)+geom_histogram(aes(x=TE.,y=..density..),fill="lightblue",color="black",binwidth=3)+geom_density(aes(x=TE.),fill="grey",alpha=0.5)+
   scale_x_continuous(name="Transfer Efficiency (%)")+
   scale_y_continuous(name="Density")+
@@ -11,3 +12,6 @@ ggplot(rusin)+geom_histogram(aes(x=TE.,y=..density..),fill="lightblue",color="bl
   geom_vline(xintercept=(mean(rusin$TE.)+2*sd(rusin$TE.)),linetype="dashed",color="red")+
   geom_vline(xintercept=(mean(rusin$TE.)-2*sd(rusin$TE.)),linetype="dashed",color="red")+
   theme_pubr()
+
+#relationship between TE and inoculation concentration?
+ggplot(rusin)+geom_point(aes(x=rusin$X5ul.Innoculum.log10,y=rusin$TE.))
